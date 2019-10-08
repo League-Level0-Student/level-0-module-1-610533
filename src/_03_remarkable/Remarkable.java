f1f5ffc033d8fef9d7e9a6084275a45fdc1b84e8
610533 <610533@0338S-S-R0B6ZHA.sdcs.local>
2019-10-08 14:29:36 -0700

    Variable Riddler

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -2,17 +2,21 @@ package _03_remarkable;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
+import java.awt.Dialog;
+
 import javax.swing.JOptionPane;
 
 public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		String Dave = "Dave"; String Remarkable = "Remarkable";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String Name = JOptionPane.showInputDialog("What is your name?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+		{if(Name.equals(Dave))
+		 JOptionPane.showMessageDialog(null, "Dave, you are Remarkable");
+		 else JOptionPane.showMessageDialog(null, "Who are you?");
 	}
 }
-
+}
--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -1,32 +1,44 @@
 package _04_secret_message_box;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set a password in a String variable
-	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+	public static void main(String[] args) {
+		String Password = "1234";
+		String Message = JOptionPane.showInputDialog("Say a secret message.");
 
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
+		String Person2 = JOptionPane.showInputDialog("Only you can see this. What's the password?");
 
-	// 4. If their guess matches the password, show them the secret message
+		if (Person2.equals(Password))
+			JOptionPane.showMessageDialog(null,Message);
+		else
+			JOptionPane.showMessageDialog(null, "INTRUDER!!");
+		// 1. Set a password in a String variable
+		
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
+		// message
+		// if they can guess the passcode
 
+		// 4. If their guess matches the password, show them the secret message
+
+		// 5. If the password does not match, pop-up "INTRUDER!!"
+
+	}
 }
